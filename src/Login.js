eee6b7bfde6e556150eee53f68cd94f707958f05
Candace Rogers <candacerogers@Candaces-MacBook-Pro.local>
2017-12-07 11:56:52 -0800

    Updated OAuth

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actionCreators from './../actions/actionCreators';
 import { Route, Redirect } from 'react-router';
-
+import Paper from 'material-ui/Paper';
 import AppBar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
@@ -13,7 +12,7 @@ import DropDownMenu from 'material-ui/DropDownMenu';
 
 function mapStateToProps(state) {
     return {
-      currentUserCandace: state.currentUserCandace
+      currentUser: state.currentUser
     }
   }
   
@@ -21,49 +20,37 @@ function mapDispatchToProps(dispatch) {
 return bindActionCreators(actionCreators, dispatch);
 }
 
-const styles = {
+const paperStyle = {
     customWidth: {
       width: 400,
+      marginLeft: 450, 
+      marginTop: 150     
     },
     display: 'inline-block'
     
   };
 
+const buttonStyle = {
+    marginLeft: 120,
+    marginTop: 15,
+    marginBottom: 15
+
+    // backgroundColor: #28a745;
+    // backgroundImage: linear-gradient(-180deg, #34d058 0%, #28a745 90%);
+};
+
+const centerStyle = {
+    // alignContent: center,
+};
+
 
 class Login extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            username: '',
-            password: ''
-        }
+
+    
         
     }
-
-    // handleClick(event) {
-    //     const apiBaseUrl = "http://localhost:3000/api";
-    //     const payload={
-    //         "username": this.state.username,
-    //         "password": this.state.password
-    //     }
-    //     axios.post(apiBaseUrl, payload)
-    //     .then(function(response) {
-    //         console.log(response);
-    //         if(response.data.code === 200) {
-    //             console.log('LOGIN SUCCESSFUL BITCH!');
-    //         }
-    //         else if(response.data.code === 204) {
-    //             console.log("WRONG PASSWORD USERNAME YO!");
-    //             alert('username and password don\'t match');
-    //         } else {
-    //             console.log('TIME TO MAKE A USERNAME YAY');
-    //             alert('Username does not exist');
-    //         }
-    //     })
-    //     .catch(function(err) {
-    //         console.log('this is the error: ', err);
-    //     });
-    // }
 
     handleGithub(event) {
         console.log('running handleGithub');
@@ -76,61 +63,50 @@ class Login extends Component {
             })
     }
 
+    stargazerOrCandace (accesstoken) {
+        console.log(accesstoken);
+        if (accesstoken === 'Candace\'s Access Token' ) {
+            this.props.currentUserCandace();
+        //   this.setState({ whoIsIt: Candace });
+        } else {
+            this.props.currentUserUser();
+        //   this.setState({ whoIsIt: User });
+        }
+      }
+    
+
     render () {
     
         return (
-            <div className>
+            <div className="Login">
                    
-                  <DropDownMenu
+                   <Paper
+                    name="myprofile"
                     onChange={this.handleChange}
-                    style={styles.customWidth}
-                    autoWidth={false}
+                    className="my-profile"
+                    style={paperStyle.customWidth}
                    >
-                    <TextField
-                      hintText="Enter you Username"
-                      floatingLabelText="Username"
-                      onChange = {(event, newValue) => this.setState({username: newValue})}
-                    />
-                    <br/>
-                    <TextField
-                      type="password"
-                      hintText="Enter you Password"
-                      floatingLabelText="Password"
-                      onChange = {(event, newValue) => this.setState({password: newValue})}
-                    />
-                    <br/>
+                   <div style={centerStyle}>
+                        <img src="./profile.jpg" alt="profile" width="150" height="auto"/>
+                        <br/>
+                        <a href="https://github.com/login/oauth/authorize?scope=user:email&amp;client_id=188d348aea165458e1d1">
+                            <RaisedButton 
+                            label="Login with GitHub" 
+                            primary={true} 
+                            style={buttonStyle} 
+                            />
+                            
+                            </a>
+                    </div>
+                </Paper>
 
-                    <RaisedButton 
-                      label="Submit" 
-                      primary={true} 
-                      style={style} 
-                      onClick={(event) => this.handleClick(event)} 
-                    />
-                   <a href="https://github.com/login/oauth/authorize?scope=user:email&amp;client_id=188d348aea165458e1d1">
-                    <RaisedButton 
-                      label="Login with GitHub" 
-                      primary={true} 
-                      style={style} 
-                    />
-                    </a>
-                    <RaisedButton
-                      label="Register"
-                      linkButton={true}
-                      primary={true} 
-                      style={style} 
-                      onClick={(event) => this.handleClick(event)} 
-                    />
-                </DropDownMenu>
                 <button onClick={this.props.currentUserCandace}>go to candace side</button>
                 <button onClick={this.props.currentUserUser}>go to user side</button>
-                {/* <img src='./../www/cats.jpg' /> */}
             </div>
         )
     }
 }
 
-const style = {
-    margin: 15,
-};
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
